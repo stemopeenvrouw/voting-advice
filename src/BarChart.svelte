96b2bb0cc967ea1e722ca30449a0ad49e878f147
Breyten Ernsting <bje@dds.nl>
2021-02-08 17:20:58 +0100

    Right color for the bar charts.

--- src/BarChart.svelte
+++ src/BarChart.svelte
@@ -9,8 +9,8 @@
 <svg viewBox="0 0 100 100" data-total="{total}" data-male="{male}" data-female="{female}">
 	<!-- <circle cx="50" cy="50" r="50" fill="#FFF" /> -->
   <g class='bars'>
-    <rect fill='#3d5599' width='100%' height='25'></rect>;
-    <rect fill='#cb4d3e' width='{Math.round(female*100/total)}%' height='25'></rect>
+    <rect fill='#dce9ef' width='100%' height='25'></rect>;
+    <rect fill='#5098AB' width='{Math.round(female*100/total)}%' height='25'></rect>
     <text in:fade={{delay: 400}} x="50" y="50" dy="0.3em" font-size="1.8rem" font-weight="bolder" fill="#ee956a" text-anchor="middle">{Math.round(female*100/total)}%</text>
   </g>
 </svg>
